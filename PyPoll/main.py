de74d8dbea9e5a73bd4d0bf5598d5b35f23844e6
cadiputra <98525195+cadiputra@users.noreply.github.com>
2022-02-28 22:46:45 +1100

    Update main.py
    Added scripts

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,31 @@
+# import modules
+import os
+import csv
 
+# specify source path
+election_csv=os.path.join("election_data.csv")
+
+# create list to store data
+Candidate=[]
+
+
+# open the csv file
+with open (election_csv,'r') as csvfile:
+    csvreader=csv.reader(csvfile,delimiter=',')
+
+    # skip header
+    header = next(csvfile)
+
+    for row in csvreader:
+
+        # list all candidate names
+        Candidate.append(row[2])
+
+# Calculate total vote
+Total_Vote = len(Candidate)
+
+# print summary
+print("Election Results")
+print("------------------------------")
+print(f'Total Votes: {Total_Vote}')
+print ("-----------------------------")
